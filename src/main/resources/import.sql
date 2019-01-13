997d485b889919af7434624be24979885a9f38b8
İlkay Günel <ilkgunel93@gmail.com>
2019-01-13 22:30:05 +0300

    Updated

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,5 @@
 insert into book(book_id, bookName, authorName) values(1, "Hayvan Çiftliği", "George Orwell");
 insert into book(book_id, bookName, authorName) values(2, "1984", "George Orwell");
---insert into book values(3,"Cesur Yeni Dünya");
---insert into book values(4,"Gurur Ve Önyargı");
+insert into book(book_id, bookName, authorName) values(3, "Cesur Yeni Dünya", "Aldous Huxley");
+insert into book(book_id, bookName, authorName) values(4, "Gurur Ve Önyargı", "Jane Austen");
+insert into book(book_id, bookName, authorName) values(5, "Beşinci Kurban", "Jane Casey");
